83ffcb3926d5f21b2d9a50dc502dca9a6b19c476
ReubenFrankel <rfrankel@matatika.com>
2021-10-17 20:25:47 +0100

    Define OAuth implementation

--- tap_spotify/auth.py
+++ tap_spotify/auth.py
@@ -1,31 +1,24 @@
 """Spotify Authentication."""
 
-
 from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
 
 
-# The SingletonMeta metaclass makes your streams reuse the same authenticator instance.
-# If this behaviour interferes with your use-case, you can remove the metaclass.
 class SpotifyAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
     """Authenticator class for Spotify."""
 
     @property
     def oauth_request_body(self) -> dict:
         """Define the OAuth request body for the Spotify API."""
-        # TODO: Define the request body needed for the API.
         return {
-            'resource': 'https://analysis.windows.net/powerbi/api',
-            'scope': self.oauth_scopes,
-            'client_id': self.config["client_id"],
-            'username': self.config["username"],
-            'password': self.config["password"],
-            'grant_type': 'password',
+            "grant_type": "refresh_token",
+            "refresh_token": self.config["refresh_token"],
+            "client_id": self.config["client_id"],
+            "client_secret": self.config["client_secret"],
         }
 
     @classmethod
     def create_for_stream(cls, stream) -> "SpotifyAuthenticator":
         return cls(
             stream=stream,
-            auth_endpoint="TODO: OAuth Endpoint URL",
-            oauth_scopes="TODO: OAuth Scopes",
+            auth_endpoint="https://accounts.spotify.com/api/token",
         )
