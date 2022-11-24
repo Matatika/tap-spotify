57660f331cfb3a4ece476ad7ae7b94db828b697c
Reuben Frankel <rfrankel@matatika.com>
2022-11-24 23:00:46 +0000

    Remove superfluous method overrides

--- tap_spotify/client.py
+++ tap_spotify/client.py
@@ -1,11 +1,10 @@
 """REST client handling, including SpotifyStream base class."""
 
-import requests
-from typing import Any, Dict, Optional, Iterable
+from typing import Any, Dict, Optional
 from urllib.parse import parse_qsl, urlsplit
 
+import requests
 from memoization import cached
-
 from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
 
@@ -26,14 +25,6 @@ class SpotifyStream(RESTStream):
         """Return a new authenticator object."""
         return SpotifyAuthenticator.create_for_stream(self)
 
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        if "user_agent" in self.config:
-            headers["User-Agent"] = self.config.get("user_agent")
-        return headers
-
     def get_next_page_token(
         self, response: requests.Response, previous_token: Optional[Any]
     ) -> Optional[Any]:
@@ -46,23 +37,3 @@ class SpotifyStream(RESTStream):
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
         return dict(parse_qsl(urlsplit(next_page_token).query))
-
-    def prepare_request_payload(
-        self, context: Optional[dict], next_page_token: Optional[Any]
-    ) -> Optional[dict]:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
-
-    def parse_response(self, response: requests.Response) -> Iterable[dict]:
-        """Parse the response and return an iterator of result rows."""
-        # TODO: Parse response body and return a set of records.
-        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
-
-    def post_process(self, row: dict, context: Optional[dict]) -> dict:
-        """As needed, append or transform raw data to match expected structure."""
-        # TODO: Delete this method if not needed.
-        return row
